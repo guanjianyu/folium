fd2cbc7f882375d943058651287ed517971b3c36
guanjianyu <jianyu.guan@justadd.ai>
2022-08-02 12:40:46 +0200

    update clickformarker

--- folium/features.py
+++ folium/features.py
@@ -1399,9 +1399,10 @@ class ClickForMarker(MacroElement):
                     var new_mark = L.marker().setLatLng(e.latlng).addTo({{this._parent.get_name()}});
                     new_mark.dragging.enable();
                     new_mark.on('dblclick', function(e){ {{this._parent.get_name()}}.removeLayer(e.target)})
-                    var lat = e.latlng.lat.toFixed(4),
-                       lng = e.latlng.lng.toFixed(4);
+                    var lat = e.latlng.lat.toFixed(6),
+                       lng = e.latlng.lng.toFixed(6);
                     new_mark.bindPopup({{ this.popup }});
+                    console.log(`${lat},${lng}`)
                     };
                 {{this._parent.get_name()}}.on('click', newMarker);
             {% endmacro %}
